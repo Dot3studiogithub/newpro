d12e47dc7fb32fe5c800e630ab3f63ebe526ad2a
Dot33studio <codemonk22233@gmail.com>
2020-03-15 21:59:08 +0530

    navbar complete

--- index.php
+++ index.php
@@ -7,55 +7,64 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/all.css">
     <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/jquery-3.4.1.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/all.js"></script>
+  <script src="js/typed.js"></script>
     
 </head>
 <body>
 <div class="container-fluid main-header22">
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">PORTFOLIO</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      
+      <li class="nav-item">
+        <a class="nav-link " href="#">ABOUT US</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link " href="#">SERVICES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">CONTACT US</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    
   </div>
 </nav>
+
+<div class="row d-flex align-items-center ">
+    <div class="col-md-8 offset-md-2 text-center my-5">
+        <div class="display-4 text-uppercase text-secondary my-3 tommy">welcome To <span class="text-warning">Dot3studio</span></div>
+        <div class="h1 text-center text-secondary my-3 text-uppercase tommy"> we are <span class="type text-danger h1"></span></div>
+    </div>
+</div>
 </div>
 
 
 
 
 
-
-  <script src="js/jquery-3.4.1.js"></script>
-  <script src="js/bootstrap.js"></script>
-  <script src="js/all.js"></script>
-  <script src="js/typed.js"></script>
+<script>
+     var typed = new Typed('.type', {
+    strings: ['DEVELOPER', 'DESIGNER'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+  });
+</script>
+  
 </body>
 </html>
